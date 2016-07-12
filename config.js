a05519d38513d16fa14555b655fbe727286df435
ravi kumar sha <ravi.sha@nagarro.com>
2016-07-12 23:44:51 +0530

    base setup

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    
+    secret: '@happnsocialnetworkingapplication#',
+    database: 'mongodb://<dbuser>:<dbpassword>@ds025762.mlab.com:25762/heroku_zm316pb7' 
+    
+};
---
+++ models/user.js
@@ -0,0 +1,3 @@
+// get an instance of mongoose and mongoose.schema
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
