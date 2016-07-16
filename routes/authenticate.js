51da6b3f701f083471e94f7efa59c8f96afb5bbb
ravi kumar sha <ravi.sha@nagarro.com>
2016-07-16 16:38:03 +0530

    message updated for authentication

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -16,7 +16,7 @@ router.post('/', function (req, res, next) {
         if(err) {throw err;}
 
         if(!user) {
-            res.json({success: false, message: 'Authentication failed. User not found.'});
+            res.json({success: false, message: 'Authentication failed. Username "' + user + '" not found.'});
             return;
         }
 
