ade0e96ff477b0361f91f8b7f2960fae6b96cfe1
ravi kumar sha <ravi.sha@nagarro.com>
2016-08-04 23:44:39 +0530

    heroku port error

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 /** Morgon to log requests to the console. */
 app.use(logger('dev'));
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 36588));
 /** Render engine setup */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
