c239fa47f27d969f0fdf31a957a46fe9c4d7c592
ravi kumar sha <ravi.sha@nagarro.com>
2016-09-12 21:31:23 +0530

    authentication failure attributes added

--- middlewares/router.js
+++ middlewares/router.js
@@ -9,13 +9,16 @@ router.use(function (req, res, next) {
      * check post parameters or url parameter or header parameter for the token.
      */
     var token = req.body.token ||  req.query.token || req.headers['x-access-token'];
-    console.log(req.headers);
-    console.log(req.headers['x-access-token']);
+
     // decode token
     if(token) {
         jwt.verify(token, config.secret, function (err, decoded) {
             if(err) {
-                return res.json({success: false, message: 'Failed to authenticate token.'});
+                return res.status(401).json({
+                  success: false,
+                  type: 'OAuthException',
+                  message: err
+                });
             }
 
             /**
